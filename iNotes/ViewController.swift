996da43154cab7dc5df5420a097dc2f8113a8db9
Emily <walker.emily010@gmail.com>
2019-12-01 16:11:27 -0600

    Initial Commit

---
+++ iNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iNotes
+//
+//  Created by Emily Walker on 12/1/19.
+//  Copyright © 2019 Emily Walker. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
